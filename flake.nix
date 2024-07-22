5b44b6e93fc196377b5030bc18bbb29fd6f19497
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2024-07-22 07:20:07 +0200

    add flake with dev shell

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "FQDN Policy";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    version = builtins.substring 0 8 (self.lastModifiedDate or self.lastModified or "19700101");
+    goOverlay = final: prev: {
+      go = prev.go.overrideAttrs (old: {
+        version = "1.22.5";
+        src = prev.fetchurl {
+          url = "https://go.dev/dl/go1.22.5.src.tar.gz";
+          hash = "sha256-rJxyPyJJaa7mJLw0/TTJ4T8qIS11xxyAfeZEu0bhEvY=";
+        };
+      });
+    };
+    withSystem = nixpkgs.lib.genAttrs ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+    withPkgs = callback:
+      withSystem (
+        system:
+          callback
+          (import nixpkgs {
+            inherit system;
+            overlays = [goOverlay];
+          })
+      );
+  in {
+    devShells = withPkgs (pkgs: {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [go gopls gotools go-tools];
+      };
+    });
+
+    formatter = withPkgs (pkgs: pkgs.nixfmt-rfc-style);
+  };
+}
